0e68d2244d53cce7e316e11e95357fe9fbc63f31
fangqi <fangqi@taihe.com>
2020-09-09 09:51:06 +0800

    同步测试

--- src/com/fq/thread/concurrent/SynchorinizedTest.java
+++ src/com/fq/thread/concurrent/SynchorinizedTest.java
@@ -22,12 +22,63 @@ public class SynchorinizedTest {
         }
     }
 
+    public synchronized void test1(){
+        while (true){
+
+        }
+    }
+
+    public synchronized void test2(){
+        System.out.println("获取对象锁");
+    }
+
+    public void test3(){
+        System.out.println("非同步方法");
+    }
+
     public static void main(String[] args) {
         SynchorinizedTest synchorinizedTest = new SynchorinizedTest();
-        new Thread(() -> {
-            for (int i = 0; i < 10000; i++) {
-                synchorinizedTest.print();
+//        new Thread(() -> {
+////            for (int i = 0; i < 10000; i++) {
+////                synchorinizedTest.print();
+////            }
+////        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchorinizedTest.test1();
             }
         }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchorinizedTest.test2();
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchorinizedTest.test3();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchorinizedTest.print();
+        }).start();
+        while (true){}
     }
 }
